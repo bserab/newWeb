0921053d43ea9172f56c077b65ee3a7dc65eb61b
bserab <chmt22074@g.nihon-u.ac.jp>
2023-12-10 22:33:31 +0900

    変更後

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
+
+  useEffect(() => {
+    const generateRandomQrCode = async () => {
+      try {
+        const text = "情報科学科, 「5422074:馬場光希」";
+        const size = 100; 
+        const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(text)}&size=${size}x${size}`);
+        setQrCodeUrl(response.url);
+      } catch (error) {
+        console.error("Error generating QR code:", error);
+      }
+    };
+
+    generateRandomQrCode();
+  }, []); 
 
   const addTask = () => {
     if (newTask.trim() !== '') {
@@ -22,7 +38,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>メモ帳</h1>
-        <h2>5422074 馬場光希</h2>
         <div>
           <input
             type="text"
@@ -40,6 +55,10 @@ function App() {
             </li>
           ))}
         </ul>
+        <div className ="maker">
+          <h2>作成者</h2>
+          {qrCodeUrl && <img src={qrCodeUrl} alt="作成者" />}
+        </div>
       </header>
     </div>
   );
